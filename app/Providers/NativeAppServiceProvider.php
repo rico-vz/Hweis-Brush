b747bc5431bdb636722a57317cccb6271df2bbb4
Rico van Zelst <rvzelst7@gmail.com>
2024-08-14 12:02:15 +0200

    refactor: update Window settings in boot method
    Adjust Window settings for better user experience.

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -7,21 +7,21 @@ use Native\Laravel\Contracts\ProvidesPhpIni;
 
 class NativeAppServiceProvider implements ProvidesPhpIni
 {
-    /**
-     * Executed once the native application has been booted.
-     * Use this method to open windows, register global shortcuts, etc.
-     */
+
     public function boot(): void
     {
-        Window::open();
+        Window::open()
+            ->width(1280)
+            ->height(720)
+            ->resizable(false)
+            ->title("Hwei's Brush")
+            ->rememberState()
+            ->titleBarHidden();
     }
 
-    /**
-     * Return an array of php.ini directives to be set.
-     */
+
     public function phpIni(): array
     {
-        return [
-        ];
+        return [];
     }
 }
